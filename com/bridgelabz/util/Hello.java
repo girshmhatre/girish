44d113d2aff61f9926360fd9fafe365cbe77c432
girish <mhatre.girish44@gmail.com>
2017-08-06 17:14:00 +0530

    shanka

--- com/bridgelabz/util/Hello.java
+++ com/bridgelabz/util/Hello.java
@@ -1,9 +1,19 @@
+
+/******************************************************************************
+ *  Purpose: print Hello.
+ *
+ *  @author  BridgeLabz
+ *  @version 1.0
+ *  @since   06-08-2017
+ *
+ ******************************************************************************/
+
 package com.bridgelabz.util;
-class Hello
+
+public class Hello
 {
-public static void main(String arg[])
-{
-System.out.println("hello java");
-}
+	public static void main(String arg[]){
+		System.out.println("hello java");
+	}
 }
 
